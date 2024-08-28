30f24f83fd77dcb27b6d5983925e0b4edc036af0
emdevelopa <gyimahsunday@gmail.com>
2024-08-29 00:36:15 +0100

    first commit

---
+++ card.js
@@ -0,0 +1,23 @@
+let parent;
+document
+  .querySelectorAll(".box")
+  .forEach((ele) => ele.addEventListener("click", toggleFlex));
+
+function toggleFlex(event) {
+  let currentParent = event.target.closest(".box");
+
+  if (parent == currentParent) return parent.classList.toggle("current");
+  parent?.classList.remove("current");
+
+  parent = currentParent;
+  parent.classList.add("current");
+}
+
+document.body.addEventListener("click", function (event) {
+  if (!event.target.closest(".box")) {
+    if (parent) {
+      parent.classList.remove("current");
+      parent = null;
+    }
+  }
+});
